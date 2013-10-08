ac6bb8f40149341a4aabe79285d52d51551c8540
alez200 <alez200@yahoo.it>
2013-10-08 18:00:03 +0200

    1

--- js/animazione.js
+++ js/animazione.js
@@ -1,11 +1,11 @@
 $(function(){
+	tempo=1;
 	init();
 	var timer = null;	
 	$(window).bind('resize', function() {  
 		if (timer) clearTimeout(timer);  
 		timer = setTimeout(init, 100);  
 	});	
-	
 });
 
 
@@ -19,7 +19,6 @@ function init() {
 	margin=(width_finestra-width_visore)/2;
 	marginFirst=((width_img-margin)*-1);
 	marginPreFirst=marginFirst-width_img;
-	
 	$(".content-copertina").css( "width",width_content_img+"px");	
 	if (marginFirst < 0) {
 		marginFirstElement=marginFirst+"px";
@@ -44,14 +43,17 @@ function init() {
 			$(this).css( "margin-left",width_box_button+"px");
 		}		
 	});
-
+	if (tempo==1) {
+		timeout = setTimeout('animazione_on();', 5000, function(){clearTimeout(timeout);});
+		
+	}	
 };
 
 function animazione_on() {
-	$(".testo").fadeOut( "slow",function(){$(".controler").remove()});
+	$(".testo").fadeOut( "fast",function(){$(".controler").remove()});
 	var details = $('.detail');
     var margin_left=(width_img*-1)+"px";
-	$(details[2]).animate({"margin-left":0},1500 ,function(){
+	$(details[2]).animate({"margin-left":0},10 ,function(){
 		$(details[0]).animate({"margin-left":margin_left},500);
 		$(details[1]).animate({"margin-left":marginFirstElement},1500,function(){
 			$('.content-copertina .detail:first').css("margin-left",0).appendTo('.content-copertina');
@@ -65,19 +67,15 @@ function animazione_back() {
     var margin_left=(width_img*-1)+"px";
 	$('.content-copertina .detail:last-child').css("margin-left",marginPreFirst+"px").prependTo('.content-copertina');
 	var details = $('.detail');
-
 	$(details[0]).animate({"margin-left":marginFirstElement},1500);
     $(details[1]).animate({"margin-left":0},1000, function(){
 		$(details[2]).animate({"margin-left":width_box_button+"px"},1000);
+		init();
 	});
-	//$(details[1]).animate({"margin-left":width_box_button+"px"},1500,function(){
-		//$('.content-copertina .detail:first').css("margin-left",0).appendTo('.content-copertina');
-		//init();
-	//});
 };
 
 function comandi(el) {
-	$("<p class='controler'><span class='back'>Dietro</span><span class='auto'> > </span><span class='on'>Avanti</span></p>").appendTo(el);
+	$("<p class='controler'><span class='auto'> > </span><span class='on'>Avanti</span></p>").appendTo(el);
 	
 	$('span.on').click(function() {
 		animazione_on();
@@ -86,6 +84,12 @@ function comandi(el) {
 		animazione_back();
 	});
 	$('span.auto').click(function() {
+		//animazione_auto();
+		alert ('pauso');
+		tempo=0;
+		clearTimeout(timeout);
+	});	
+	$('span.pausa').click(function() {
 		//animazione_auto();
 		alert ('Automatizzo');
 	});	
